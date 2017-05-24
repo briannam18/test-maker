5d05b3b8584ba424d2ccecf54865d8c4332f0e4b
mattang687 <mattang687@gmail.com>
2017-05-24 13:34:26 -0400

    Update Screen.java
    Made an array to store question type, difficulty, and number. Fixed left side of text being cut off

--- Screen.java
+++ Screen.java
@@ -30,7 +30,7 @@ public class Screen extends JFrame{
 	private String [] questionChoices = {"Multiple Choice", "Fill in the Blank", "Objective Based"};
 	private String [] difficulties = {"1","2","3","4","5"};
 	private String [] amounts = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25", "26","27","28","29","30"};
-	
+	public String[] output = new String[3];
 	
 	public Screen(){
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -72,7 +72,7 @@ public class Screen extends JFrame{
         
         // Set West Panel
         JButton add = new JButton("Add");
-        add.setPreferredSize(new Dimension(80,80));
+        add.setPreferredSize(new Dimension(150,150));
         add.addActionListener(new ActionListener() { 
         	  public void actionPerformed(ActionEvent e) { 
         	    addButtonPressed();
@@ -103,7 +103,7 @@ public class Screen extends JFrame{
 		text = new JTextArea();
 		text.setFont(text.getFont().deriveFont(15f));
 		JScrollPane scrollPane = new JScrollPane(text);
-		scrollPane.setPreferredSize(new Dimension(1000,getHeight()-200));
+		scrollPane.setPreferredSize(new Dimension(getWidth()*7/10,getHeight()-200));
 		
 		east.setBorder(BorderFactory.createEmptyBorder(getHeight()/20, 0, getHeight()/10, getWidth()/20));
 		west.setBorder(BorderFactory.createEmptyBorder(getHeight()/20, getWidth()/20, getHeight()/10, getWidth()/20));
@@ -123,7 +123,9 @@ public class Screen extends JFrame{
 		text.append(boxes[0].getSelectedItem().toString() + '\n');
 		text.append(boxes[1].getSelectedItem().toString() + '\n');
 		text.append(boxes[2].getSelectedItem().toString() + '\n');
-		
+		output[0] = boxes[0].getSelectedItem().toString();
+		output[1] = boxes[1].getSelectedItem().toString();
+		output[2] = boxes[2].getSelectedItem().toString();
 	}
 	
 	
